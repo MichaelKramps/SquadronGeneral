302358fba51e90080f5d70dc3ddeb20f225ee944
Michael Kramps <mdk989@gmail.com>
2015-07-08 19:29:22 -0500

    verified mongoDB connection

--- server.js
+++ server.js
@@ -42,6 +42,11 @@ app.use(cookieParser());
 /**************** Set Routes ****************/
 
 mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log("yay!");
+});
 
 io.on('connection', function (socket) {
   console.log("user connected")
