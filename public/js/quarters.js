76dc3f1ac8b422db527495f48afb9112b3c16037
Michael Kramps <mdk989@gmail.com>
2015-08-26 15:21:24 -0500

    working on main menu

--- public/js/quarters.js
+++ public/js/quarters.js
@@ -5,11 +5,20 @@ var quarters = {
     },
 
     create: function () {
-        var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+        var style = { font: game.world._height * 0.05 + "px Arial", fill: "#ff0044", align: "center" };
+        
+        // Player Info
+        var quartersHeader = game.add.text(game.world._width * 0.05, game.world._height * 0.1, player.username + "'s Quarters", style);
+        quartersHeader.anchor.set(0);
+        
+        var rank = game.add.text(game.world._width * 0.05, game.world._height * 0.2, "Rank: ", style);
+        rank.anchor.set(0);
+        
+        var tournamentPoints = game.add.text(game.world._width * 0.05, game.world._height * 0.3, "Tournament Points: ", style);
+        tournamentPoints.anchor.set(0);
+        
+        // Change Game States
 
-        var text = game.add.text(game.world.centerX, game.world.centerY, player.username + "'s Quarters", style);
-
-        text.anchor.set(0.5);
-    }
+    },
 
 };
